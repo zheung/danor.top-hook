9d6d8f85523ffdd28f19171d3b96c728870f46b6
DanoR <zheung@vip.qq.com>
2017-05-04 10:13:30 +0800

    update shell script

--- webhook.sh
+++ webhook.sh
@@ -9,6 +9,24 @@ ssh-add ~/.ssh/kimg 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
 
 ssh-add -l 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
 
+cd ~/serv/_
+
+git clean -fd 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+git checkout -- . 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+git pull github master 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+
+cd ~/serv/pub
+
+git clean -fd 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+git checkout -- . 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+git pull github master 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+
+cd ~/serv/hook
+
+git clean -fd 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+git checkout -- . 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+git pull github master 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+
 cd ~/serv/serv/kq
 
 git clean -fd 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
@@ -26,6 +44,7 @@ cd ~/serv
 git clean -fd 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
 git checkout -- . 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
 git pull github master 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+
 npm i 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
 
 fr start -o ~/logs/kq/out.log -e ~/logs/kq/err.log /root/serv/index.js 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
