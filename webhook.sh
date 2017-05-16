0cb54e51ec6d80492a1ddd398efe3ecb0f23fd64
DanoR <zheung@vip.qq.com>
2017-05-16 16:50:27 +0800

    no ~

--- webhook.sh
+++ webhook.sh
@@ -1,50 +1,50 @@
-date +"%y-%m-%d %H:%M:%S" 1>~/logs/kq/bsh.log
+date +"%y-%m-%d %H:%M:%S" 1>/root/logs/kq/bsh.log
 
-pm2 stop /root/serv/index.js 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+pm2 stop /root/serv/index.js 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
 
-eval `ssh-agent` 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+eval `ssh-agent` 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
 
-ssh-add ~/.ssh/kqpu 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
-ssh-add ~/.ssh/kimg 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+ssh-add /root/.ssh/kqpu 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
+ssh-add /root/.ssh/kimg 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
 
-ssh-add -l 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+ssh-add -l 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
 
-cd ~/serv/serv/_
+cd /root/serv/serv/_
 
-git clean -fd 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
-git checkout -- . 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
-git pull github master 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+git clean -fd 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
+git checkout -- . 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
+git pull github master 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
 
-cd ~/serv/serv/pub
+cd /root/serv/serv/pub
 
-git clean -fd 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
-git checkout -- . 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
-git pull github master 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+git clean -fd 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
+git checkout -- . 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
+git pull github master 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
 
-cd ~/serv/serv/hook
+cd /root/serv/serv/hook
 
-git clean -fd 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
-git checkout -- . 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
-git pull github master 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+git clean -fd 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
+git checkout -- . 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
+git pull github master 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
 
-cd ~/serv/serv/kq
+cd /root/serv/serv/kq
 
-git clean -fd 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
-git checkout -- . 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
-git pull github master 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+git clean -fd 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
+git checkout -- . 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
+git pull github master 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
 
-cd ~/serv/serv/af
+cd /root/serv/serv/af
 
-git clean -fd 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
-git checkout -- . 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
-git pull github master 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+git clean -fd 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
+git checkout -- . 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
+git pull github master 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
 
-cd ~/serv
+cd /root/serv
 
-git clean -fd 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
-git checkout -- . 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
-git pull github master 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+git clean -fd 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
+git checkout -- . 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
+git pull github master 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
 
-npm i 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+npm i 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
 
-pm2 start /root/serv/index.js 1>>~/logs/kq/bsh.log 2>>~/logs/kq/bsh.log
+pm2 start /root/serv/index.js 1>>/root/logs/kq/bsh.log 2>>/root/logs/kq/bsh.log
